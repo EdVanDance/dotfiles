749ab14bee497330177eefbb7b48e2b8cb8d1830
EdVanDance <EdVanDance@gmail.com>
2022-09-23 15:51:24 +0200

    git: Add fetch and show stat aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,6 @@
 alias g=git
 alias ga='git add'
+alias gf='git fetch --prune'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%C(red)%h%C(reset) %s %C(cyan)@%an%C(reset)%C(yellow)%d %C(reset)%C(green)(%cr)%C(reset)' --abbrev-commit --date=relative"
 alias gp='git push'
@@ -12,7 +13,9 @@ alias gca='git commit -av'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb'
+alias gss="git show --stat --pretty=format:'%C(red)%h%C(reset) %s %C(cyan)@%an%C(reset)%C(yellow)%d %C(reset)%C(green)(%cr)%C(reset)' --abbrev-commit"
 alias gsu='git submodule update'
+
 alias gbak='git push --mirror backup'
 
 # git-svn aliases
