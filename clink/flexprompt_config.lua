63e345b4300cc290a202855eef79ec3aedac9afa
EdVanDance <EdVanDance@gmail.com>
2023-02-27 12:59:01 +0100

    clink: use clink-flex-prompt

---
+++ clink/flexprompt_config.lua
@@ -0,0 +1,28 @@
+flexprompt = flexprompt or {}
+flexprompt.settings = flexprompt.settings or {}
+flexprompt.settings.connection = "disconnected"
+flexprompt.settings.lines = "two"
+flexprompt.settings.symbols =
+{
+    prompt =
+    {
+        ">",
+        winterminal = "❯",
+    },
+}
+flexprompt.settings.powerline_font = true
+flexprompt.settings.heads = "pointed"
+flexprompt.settings.spacing = "compact"
+flexprompt.settings.frame_color = "darkest"
+flexprompt.settings.right_prompt = "{exit}{duration}"
+flexprompt.settings.flow = "concise"
+flexprompt.settings.use_icons = true
+flexprompt.settings.left_prompt = "{battery}{histlabel}{cwd}{git}"
+flexprompt.settings.charset = "unicode"
+flexprompt.settings.style = "lean"
+flexprompt.settings.left_frame = "round"
+flexprompt.settings.right_frame = "none"
+flexprompt.settings.lean_separators = "dot"
+flexprompt.settings.use_8bit_color = true
+
+flexprompt.settings.left_prompt = flexprompt.settings.left_prompt:gsub("{cwd}", "{cwd:color=blue}")
