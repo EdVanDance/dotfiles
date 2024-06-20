460a5d149e5570b95418910bf6214ebe18bfc9cb
EdVanDance <EdVanDance@gmail.com>
2024-06-20 21:30:28 +0200

    zsh: new prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -12,22 +12,22 @@ else
 fi
 
 git_color () {
-  if $git diff --quiet --ignore-submodules HEAD 2>&1 ; then
-    echo "%{$fg[green]%}$1%{$reset_color%}"
+  if $git diff --quiet --ignore-submodules HEAD 2>/dev/null ; then
+    echo "%F{green}$1%f"
   else
-    echo "%{$fg[red]%}$1%{$reset_color%}"
+    echo "%F{red}$1%f"
   fi
 }
 
 git_untracked () {
-  if $git status --porcelain | grep -q '^??' ; then
-    echo "%{$fg[magenta]%}?%{$reset_color%}"
+  if $git status --porcelain 2>/dev/null | grep -q '^??' ; then
+    echo "%F{magenta}?%f"
   fi
 }
 
 git_need_push () {
   if [[ $($git cherry -v @{upstream} 2>/dev/null) != "" ]] ; then
-    echo "%{$fg[magenta]%}^%{$reset_color%}"
+    echo "%F{magenta}^%f"
   fi
 }
 
@@ -38,29 +38,28 @@ git_prompt () {
   ref=$($git symbolic-ref HEAD 2>/dev/null) || return
 
   ref="${ref#refs/heads/}"
-  echo "[$(git_need_push)$(git_color $ref)$(git_untracked)]"
+  echo " · $(git_need_push)$(git_color $ref)$(git_untracked)"
 }
 
 directory_name () {
-  echo "%{$fg[cyan]%}%3~%{$reset_color%}"
+  echo "%F{12} %~%f"
 }
 
 prompt_prefix () {
   local prefix
-  prefix=""
+  prefix="%F{242}╭─ %f"
 
   if [[ "$SSH_CONNECTION" != "" ]] ; then
     prefix="(@%{$fg[yellow]%}%m%{$reset_color%}) "
   fi
 
-  prefix=$prefix"%{$fg[green]%}~>%{$reset_color%}"
   echo $prefix
 }
 
-export PROMPT=$'$(prompt_prefix) $(directory_name) › '
-export RPROMPT='$(git_prompt)'
+export PROMPT=$'$(prompt_prefix)$(directory_name)$(git_prompt)\n%F{242}╰─ %f› '
+export RPROMPT=''
 if (( $+commands[rbenv] )) ; then
-  export RPROMPT=$RPROMPT'(%{$fg[yellow]%}$(rbenv version-name)%{$reset_color%})'
+  export RPROMPT=$RPROMPT'(%F{yellow}$(rbenv version-name)%f)'
 fi
 
 precmd () {
